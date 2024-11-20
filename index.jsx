a366264a032d94259092d3173f0345380f19c7da
avabrb <apb7@rice.edu>
2024-11-19 18:19:05 -0600

    files

---
+++ index.jsx
@@ -0,0 +1,233 @@
+import React from "react";
+import Hero1 from "./227757-hero-1.png";
+import eventIcon2 from "./event-icon-2.png";
+import eventIcon from "./event-icon.png";
+import group18 from "./group-18.png";
+import group19 from "./group-19.png";
+import image from "./image.png";
+import jocylyn from "./jocylyn.png";
+import maskGroup from "./mask-group.png";
+import pexelsDivinetechygirl11814381 from "./pexels-divinetechygirl-1181438-1.png";
+import pexelsEkaterinaBolovtsova60770891 from "./pexels-ekaterina-bolovtsova-6077089-1.png";
+import sara from "./sara.png";
+import "./style.css";
+import unnamed8RemovebgPreview12 from "./unnamed-8-removebg-preview-1-2.png";
+import unnamed8RemovebgPreview1 from "./unnamed-8-removebg-preview-1.png";
+import unnamed8RemovebgPreview2 from "./unnamed-8-removebg-preview-2.png";
+import vector1 from "./vector-1.svg";
+
+export const HomePage = () => {
+    return (
+        <div className="home-page">
+            <div className="overlap">
+                <div className="rectangle" />
+
+                <div className="div" />
+
+                <div className="meet-the-team">
+                    Meet the <br />
+                    team:
+                </div>
+
+                <div className="frame">
+                    <img className="img" alt="Sara" src={sara} />
+
+                    <img className="img" alt="Jocylyn" src={jocylyn} />
+                </div>
+
+                <img className="jocylyn" alt="Jocylyn" src={image} />
+
+                <p className="we-empower-FGLI">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    We empower FGLI students to pursue their dreams in law. Through
+                    mentorship, resources, and a supportive community, we help members
+                    overcome unique challenges.
+                </p>
+
+                <img
+                    className="unnamed-removebg"
+                    alt="Unnamed removebg"
+                    src={unnamed8RemovebgPreview12}
+                />
+
+                <div className="text-wrapper">More about us</div>
+            </div>
+
+            <div className="overlap-group">
+                <div className="overlap-2">
+                    <div className="rectangle-2" />
+
+                    <div className="rectangle-3" />
+
+                    <div className="rectangle-4" />
+
+                    <div className="rectangle-5" />
+
+                    <div className="text-wrapper-2">About Us</div>
+
+                    <img
+                        className="pexels"
+                        alt="Pexels"
+                        src={pexelsDivinetechygirl11814381}
+                    />
+
+                    <div className="group">
+                        <img className="mask-group" alt="Mask group" src={maskGroup} />
+                    </div>
+
+                    <p className="a-space-for-FGLI">
+                        <span className="span">A Space for </span>
+
+                        <span className="text-wrapper-3">FGLI</span>
+
+                        <span className="span"> Prelaw Students</span>
+                    </p>
+
+                    <div className="text-wrapper-4">Upcoming Events:</div>
+
+                    <div className="text-wrapper-5">Learn about past events</div>
+
+                    <div className="text-wrapper-6">About Us:</div>
+
+                    <div className="text-wrapper-7">FGLC</div>
+
+                    <div className="group-2">
+                        <div className="navbar-wrapper">
+                            <div className="navbar">
+                                <div className="text-wrapper-8">Resources</div>
+
+                                <div className="text-wrapper-9">Events</div>
+
+                                <div className="text-wrapper-10">Our Team</div>
+
+                                <div className="text-wrapper-11">About</div>
+                            </div>
+                        </div>
+
+                        <p className="p">
+                            We empower FGLI students to pursue their dreams in law. Through
+                            mentorship, resources, and a supportive community, we help members
+                            overcome unique challenges. Join us to take your first steps
+                            toward a meaningful future in law.
+                        </p>
+
+                        <p className="text-wrapper-12">Start building your prelaw future</p>
+
+                        <p className="text-wrapper-13">
+                            We empower FGLI students to pursue their dreams in law. Through
+                            mentorship, resources, and a supportive community, we help members
+                            overcome unique challenges. Join us to take your first steps
+                            toward a meaningful future in law.
+                        </p>
+                    </div>
+
+                    <div className="frame-2" />
+
+                    <div className="group-3">
+                        <img
+                            className="unnamed-removebg-2"
+                            alt="Unnamed removebg"
+                            src={unnamed8RemovebgPreview1}
+                        />
+
+                        <div className="text-wrapper-14">F G L C</div>
+                    </div>
+
+                    <img className="vector" alt="Vector" src={vector1} />
+
+                    <img className="event-icon" alt="Event icon" src={eventIcon} />
+
+                    <img className="event-icon-2" alt="Event icon" src={eventIcon2} />
+
+                    <div className="group-4">
+                        <div className="text-wrapper-15">Accessing Resources:</div>
+
+                        <div className="frame-3">
+                            <div className="overlap-group-wrapper">
+                                <div className="overlap-group-2">
+                                    <img
+                                        className="pexels-ekaterina"
+                                        alt="Pexels ekaterina"
+                                        src={pexelsEkaterinaBolovtsova60770891}
+                                    />
+
+                                    <img className="group-5" alt="Group" src={group19} />
+                                </div>
+                            </div>
+
+                            <p className="we-empower-FGLI-2">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We
+                                empower FGLI students to pursue their dreams in law. Through
+                                mentorship, resources, and a supportive community, we help
+                                members overcome unique challenges. Join us to take your first
+                                steps toward a meaningful future in law. We empower FGLI
+                                students to pursue their dreams in law. Through mentorship,
+                                resources, and a supportive community, we help members overcome
+                                unique challenges.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="group-6">
+                        <div className="mentorship-community">
+                            Mentorship &amp; Community:
+                        </div>
+
+                        <div className="frame-4">
+                            <p className="we-empower-FGLI-3">
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We
+                                empower FGLI students to pursue their dreams in law. Through
+                                mentorship, resources, and a supportive community, we help
+                                members overcome unique challenges. Join us to take your first
+                                steps toward a meaningful future in law. We empower FGLI
+                                students to pursue their dreams in law. Through mentorship,
+                                resources, and a supportive community, we help members overcome
+                                unique challenges.
+                            </p>
+
+                            <div className="div-wrapper">
+                                <div className="overlap-group-3">
+                                    <img
+                                        className="element-hero"
+                                        alt="Element hero"
+                                        src={Hero1}
+                                    />
+
+                                    <img className="group-7" alt="Group" src={group18} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="text-wrapper-16">Join Us</div>
+                </div>
+
+                <div className="group-8">
+                    <div className="text-wrapper-17">Secretary</div>
+                </div>
+            </div>
+
+            <div className="overlap-wrapper">
+                <div className="overlap-3">
+                    <div className="text-wrapper-18">
+                        First Generation Legal Collective
+                    </div>
+
+                    <div className="text-wrapper-19">Email us: FGLC@rice.edu</div>
+
+                    <div className="text-wrapper-20">Instagram: FGLC</div>
+
+                    <div className="text-wrapper-21">Rice University</div>
+
+                    <div className="text-wrapper-22">2024</div>
+
+                    <img
+                        className="unnamed-removebg-3"
+                        alt="Unnamed removebg"
+                        src={unnamed8RemovebgPreview2}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
